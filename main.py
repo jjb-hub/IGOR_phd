844d4bc7c437f6782c97184c5aec41ac765b08ba
jjb-hub <120100465+jjb-hub@users.noreply.github.com>
2023-11-24 19:17:20 +0100

    created main for importing user accessable functions and iniate filesystem which i will be building now

---
+++ main.py
@@ -0,0 +1,7 @@
+######## ONLY IMPORT THE USER ACCESSIBLE FUNCTIONS HERE ##########
+
+
+
+######## INIT ##########
+# Start by checking filesystem has all the folders necessary for read/write operations (cache) or create them otherwise
+# initiateFileSystem()
